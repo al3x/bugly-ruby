a5a5e478aac2bf221b33d7dbdd5d0c886c628855
Alex Payne <al3x@al3x.net>
2011-03-07 12:50:11 -0800

    fetching specific milestones

--- spec/bugly/milestone_spec.rb
+++ spec/bugly/milestone_spec.rb
@@ -13,4 +13,13 @@ describe Bugly::Milestone do
     perfect.should be_kind_of(Bugly::Milestone)
     perfect.name.should == "it's perfect"
   end
+  
+  it "fetches a specific milestone" do
+    stub_get('/milestones/154.xml', 'milestone_154.xml')
+    
+    milestone = account.milestone(154)
+    milestone.should be_kind_of(Bugly::Milestone)
+    milestone.id.should == 154
+    milestone.name.should == "it's perfect"
+  end
 end
